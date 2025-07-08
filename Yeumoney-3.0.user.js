72c03a357a9161012459e1120cbca250079ed38c
hoanqson2107 <chidongianlason@gmail.com>
2025-07-08 18:53:27 +0700

    Update Yeumoney-3.0.user.js

--- Yeumoney-3.0.user.js
+++ Yeumoney-3.0.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         Yeumoney Bypass Loader (Obfuscated)
+// @name         Yeumoney Bypass 
 // @namespace    http://tampermonkey.net/
-// @version      2.0
+// @version      3.0
 // @author       HoanqSon2107
 // @match        https://yeumoney.com/*
 // @exclude      https://yeumoney.com/quangly/check_code.php*
 // @require      https://raw.githubusercontent.com/Hoanqson/Yeumoney-/refs/heads/main/Yeumoney.js
-// @grant        none
+// @icon        https://i.postimg.cc/wTkSR8Y6/2360a26a6dba285a17b39f66b5715273.jpg
 // ==/UserScript==
